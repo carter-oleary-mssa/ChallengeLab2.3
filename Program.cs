76ef45a817c5524c76c49c4be3c539cefabfb334
carter-oleary <carter.m.oleary@outlook.com>
2025-04-16 15:34:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter a number: ");
+string display = Console.ReadLine();
+Console.Write("Enter the desired width: ");
+int width = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i < width; i++)
+{
+    int row = width - i;
+    for(int j = 0; j < row; j++)
+    {
+        Console.Write(display);
+    }
+    Console.WriteLine();
+}
